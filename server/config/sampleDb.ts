179ceccfd2fc6528beae3939d0d44eef478225b8
luca aeberhard <luca.aeberhard@gmx.net>
2022-01-09 17:51:18 +0100

    changed pictures

--- server/config/sampleDb.ts
+++ server/config/sampleDb.ts
@@ -6,7 +6,7 @@ products.set('1', {
   title: 'Appenzeller Classic',
   description: 'Schweizer Halbhartkäse und  vollfett. aus Rohmilch',
   price: 3.45,
-  thumbnail: './assets/appenzeller.jpg',
+  thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Appenzeller_%28cheese%29.jpg',
   rating: 5,
   category: '2',
 });
@@ -16,7 +16,7 @@ products.set('2', {
   title: 'Eier',
   description: '9 Schweizer Eier aus Frilandhaltung',
   price: 5.40,
-  thumbnail: './assets/eier.jpg',
+  thumbnail: 'https://image.geo.de/30121054/t/vQ/v4/w1440/r0/-/eier-is-547420626-jpg--68255-.jpg',
   rating: 5,
   category: '2',
 });
@@ -26,7 +26,7 @@ products.set('3', {
   title: 'Iced Green Tea',
   description: 'AriZona Green Tea - Grünteegrtränk',
   price: 10.80,
-  thumbnail: './assets/icedtea.jpg',
+  thumbnail: 'https://cdn.competec.ch/images2/2/2/9/188649922/188649922_xxl3.jpg',
   rating: 5,
   category: '1',
 });
@@ -36,7 +36,7 @@ products.set('4', {
   title: 'Kalbs-Bratwuerste',
   description: 'Terra Suisse Kalbs-Bratwurst 3x2 Stück',
   price: 16.50,
-  thumbnail: './assets/kalbsbratwuerste.jpg',
+  thumbnail: 'https://t3.ftcdn.net/jpg/02/75/83/98/360_F_275839832_YJHxCK4yVOHmt0tUM3Kgb1FrvxICIzJh.jpg',
   rating: 5,
   category: '2',
 });
@@ -46,7 +46,7 @@ products.set('5', {
   title: 'Krustenkranz',
   description: 'Terra Suisse',
   price: 2.30,
-  thumbnail: './assets/krustenkranz.jpg',
+  thumbnail: 'https://www.naturesuisse.ch/fileadmin/user_upload/Panetteria_det_3.jpg',
   rating: 5,
   category: '2',
 });
@@ -56,7 +56,7 @@ products.set('6', {
   title: 'Nektarinen gelb',
   description: 'Herkunft: Spanien',
   price: 5.20,
-  thumbnail: './assets/nektarinen.jpg',
+  thumbnail: 'https://lebensmittel-warenkunde.de/assets/images/nektarine.jpg',
   rating: 5,
   category: '2',
 });
@@ -66,7 +66,7 @@ products.set('7', {
   title: 'Olivenoel',
   description: 'Bertolli Olivenöl extra vergine originale',
   price: 17.95,
-  thumbnail: '../assets/olivenoel.jpg',
+  thumbnail: 'https://www.coop.ch/img/produkte/737_737/RGB/5823140_001.jpg?_=1581300082507',
   rating: 5,
   category: '1',
 });
@@ -76,7 +76,7 @@ products.set('8', {
   title: 'Senf',
   description: 'Senf mild',
   price: 3.40,
-  thumbnail: './assets/senf.jpg',
+  thumbnail: 'https://swissmade.direct/wp-content/uploads/2017/05/Thomy-Mild-Mustard-200-g.jpg',
   rating: 5,
   category: '1',
 });
@@ -86,7 +86,7 @@ products.set('9', {
   title: 'Rispentomaten',
   description: 'Tomaten verfügen über einen hohen Gehalt an Vitamin C sowie Zucker und Mineralstoffen.',
   price: 3.10,
-  thumbnail: 'assets/tomaten.jpg',
+  thumbnail: 'https://www.pastaweb.de/wp-content/uploads/2016/02/tomaten.jpg',
   rating: 5,
   category: '2',
 });
@@ -96,7 +96,7 @@ products.set('10', {
   title: 'Magunm Almond',
   description: 'Vanilleglace und Milchschokolade mit Mandeln',
   price: 9.90,
-  thumbnail: './assets/vanille_glace.jpg',
+  thumbnail: 'https://www.lusso-business.ch/shop/media/image/1e/08/48/Magnum_1470x2000px_2021_Mandel_600x441@2x.jpg',
   rating: 5,
   category: '2',
 });
